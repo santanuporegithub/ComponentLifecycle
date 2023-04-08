08f051d9b6990897742d55de29a1887b2ca60eaf
Shan <shanpore@gmail.com>
2023-04-08 14:42:14 +0000

    Comonent lifecycle

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {Component} from 'react'
+import Clock from './components/Clock'
+
+import './App.css'
+
+class App extends Component {
+  state = {showClock: false}
+
+  onToggle = () => {
+    this.setState(prevState => {
+      const {showClock} = prevState
+      return {
+        showClock: !showClock,
+      }
+    })
+  }
+
+  render() {
+    const {showClock} = this.state
+    return (
+      <div className="app-container">
+        <button type="button" className="toggle-button" onClick={this.onToggle}>
+          {showClock ? 'Hide Clock' : 'Show Clock'}
+        </button>
+        {showClock && <Clock />}
+      </div>
+    )
+  }
+}
+
+export default App
